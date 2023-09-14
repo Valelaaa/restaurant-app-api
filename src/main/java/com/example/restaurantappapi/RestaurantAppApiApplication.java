1c447cfb5989911bf7fd74b97effd86429ba4796
Valelaaa <kosneanuvaleriy@gmail.com>
2023-09-14 18:52:26 +0300

    first commit

---
+++ src/main/java/com/example/restaurantappapi/RestaurantAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurantappapi/RestaurantAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
